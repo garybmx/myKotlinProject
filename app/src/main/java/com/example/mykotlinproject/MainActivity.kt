d6e81fd60d1c8906c5f1c8df230a85e4c5315a97
Igor U <garymtb@yandex.ru>
2019-02-28 16:44:10 +0300

    1. Создать новый проект в Android studio без поддержки Kotlin. 2. Сконфигурировать Kotlin в новом проекте (лучше вручную). 3. Перевести MainActivity на Kotlin. 4. Добавить кнопку в разметку и повесить на нее clickListener в Activity. 5. Убедиться, что все работает.

---
+++ app/src/main/java/com/example/mykotlinproject/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.mykotlinproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.kotlin_button);
+        button.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Hello!", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
